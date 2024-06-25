ecfb64bc95b66a7eb3e51036be3eae64611dba92
happyjonnacute <losnijmik@gmail.com>
2024-06-26 00:18:52 +0900

    C:/Program Files/Git/usr/article/write 명령어 입력 구현

--- src/main/java/com/keh/java/text/board/Main.java
+++ src/main/java/com/keh/java/text/board/Main.java
@@ -12,14 +12,22 @@ public class Main {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("exit")){
+      if (cmd.equals("/usr/article/write")) {
+        System.out.print("제목 : ");
+        String title = sc.nextLine();
+
+        System.out.print("내용 : ");
+        String content = sc.nextLine();
+
+        int id = 1;
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+      } else if (cmd.equals("exit")) {
+        System.out.println("== 자바 텍스트 게시판 종료 ==");
         break;
+      } else {
+        System.out.println("올바른 명령어가 아닙니다.");
       }
-
-      System.out.printf("입력받은 명령어 : %s\n", cmd);
     }
-
-    System.out.println("== 자바 텍스트 게시판 종료 ==");
 
     sc.close();
   }
