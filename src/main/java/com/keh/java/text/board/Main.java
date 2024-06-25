d33449496af43bb09910e38222d26b6e88ef2ac7
happyjonnacute <losnijmik@gmail.com>
2024-06-25 23:45:56 +0900

    프로그램 시작 메시지

--- src/main/java/com/keh/java/text/board/Main.java
+++ src/main/java/com/keh/java/text/board/Main.java
@@ -2,6 +2,10 @@ package com.keh.java.text.board;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("== 자바 텍스트 게시판 ==");
+    System.out.println("프로그램 시작");
+    System.out.println("프로그램 끝");
+    System.out.println("== 자바 텍스트 종료 ==");
+
   }
 }
